5efbad7c7e84ca833c8465c9370954446eb118c2
Azaan Ajmal Khan <roghani7488@gmail.com>
2026-01-03 16:44:25 +0500

    first commit

---
+++ error handling/script.js
@@ -0,0 +1,46 @@
+
+    // let a = window.location
+
+    // console.log(a);
+    
+
+    // window.setTimeout(function(){
+    //     console.log("Hallow World");
+        
+    //     window.location.href = "https://youtube.com/"
+
+    // }, 3000)
+
+
+
+
+
+    // window.setTimeout(function(){
+        // console.log("Hallow World");
+        
+        // window.location.href = "https://youtube.com/"
+
+    //     window.open("https://youtube.com/", "_blank")
+
+    // }, 3000)
+
+
+
+//    let id = window.setInterval(function(){
+        // console.log("Hallow World");
+        
+        // window.location.href = "https://youtube.com/"
+
+//         window.open("https://youtube.com/", "_blank")
+
+//     }, 3000)
+
+// clearInterval(id)
+
+function abc(){
+
+    alert("okay")
+
+    window.close()
+    
+}
